dbfd8a5964e78b5f0d88b93927def0f3e4777b73
AL-Amin008 <mamin.193008@bscse.uiu.ac.bd>
2023-08-28 22:53:29 +0600

    code

--- server.js
+++ server.js
@@ -1,1 +1,30 @@
-AL-Amin008
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.end('This is Home Page\n');
+    } else if (req.url === '/about') {
+        res.end('This is About Page\n');
+    } else if (req.url === '/contact') {
+        res.end('This is Contact Page\n');
+    } else if (req.url === '/file-write') {
+        fs.writeFile('demo.txt', 'hello world', (err) => {
+            if (err) {
+                res.statusCode = 500;
+                res.end('Error writing to file\n');
+            } else {
+                res.end('File created and text written\n');
+            }
+        });
+    } else {
+        res.statusCode = 404;
+        res.end('404 Not Found\n');
+    }
+});
+
+const PORT = 3000;
+
+server.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
